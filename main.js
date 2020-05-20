5a24022bd74c3c985f0cd5c3e1a5b520517ebc37
Elizabeth Bryson <info@evangalist.agency>
2020-05-19 20:45:10 -0500

    create calculator object and prevent update if button val length is greater than one when a user drags/drops

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
-let currentValue = 0;
-const calcDisplay  = document.getElementById('calcDisplay');
-const calcButtons = document.querySelector('.calculator-nums');
+// Calculator Object & Methods
+let calculator = {
+    currentValue : 0,
+    calcDisplay : document.getElementById('calcDisplay'),
+    calcButtons : document.querySelector('.calculator-nums'),
+    clearDisplay() {
+        calcDisplay.innerHTML = 0;
+    },
+    undoDisplay() {
+        const currentVal = calcDisplay.innerHTML;
+        const newVal = currentVal.slice(0, currentVal.length - 1 );
+        calcDisplay.innerHTML = newVal;
+    },
+    updateDisplay(val) {
+        calcDisplay.innerHTML = (calcDisplay.innerHTML === '0') ? val : (calcDisplay.innerHTML + val);
+    }
+}
 
-calcButtons.addEventListener('click', function(e) {
+// Event Listener for Number Buttons
+calculator.calcButtons.addEventListener('click', function(e) {
     let buttonVal = e.target.innerHTML;
-    if (e.target.innerHTML === 'C') {
-        clearDisplay();
-    } else {
-        updateDisplay(buttonVal);
+    // Prevent Accidential Drag/Drop
+    if (buttonVal.length === 1) {
+        if (e.target.innerHTML === 'C') {
+            calculator.clearDisplay();
+        } else if (e.target.innerHTML == '&lt;-') {
+            calculator.undoDisplay();
+        } else {
+            calculator.updateDisplay(buttonVal);
+        }
     }
 });
 
-function clearDisplay() {
-    calcDisplay.innerHTML = 0;
-}
-function updateDisplay(val) {
-    calcDisplay.innerHTML = calcDisplay.innerHTML + val;
-}
+// Event Listener for Operator Buttons
+
+
