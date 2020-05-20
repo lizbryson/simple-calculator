5760448ae50f54c36361640a8b71ed786c09b703
Elizabeth Bryson <info@evangalist.agency>
2020-05-19 20:30:25 -0500

    event listener for number buttons & clear button

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-const calcButtons = document.querySelector('.calculator');
+let currentValue = 0;
+const calcDisplay  = document.getElementById('calcDisplay');
+const calcButtons = document.querySelector('.calculator-nums');
 
-calcButtons.addEventListener('click', function(event) {
-    console.log(event.target);
-})
+calcButtons.addEventListener('click', function(e) {
+    let buttonVal = e.target.innerHTML;
+    if (e.target.innerHTML === 'C') {
+        clearDisplay();
+    } else {
+        updateDisplay(buttonVal);
+    }
+});
 
+function clearDisplay() {
+    calcDisplay.innerHTML = 0;
+}
+function updateDisplay(val) {
+    calcDisplay.innerHTML = calcDisplay.innerHTML + val;
+}
