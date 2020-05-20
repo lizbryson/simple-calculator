7a77f79353b061af6d4eb2bfec9e1e1bc05a1a29
Elizabeth Bryson <info@evangalist.agency>
2020-05-20 14:05:27 -0500

    additional js work

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
 // Calculator Object & Methods
 let calculator = {
     currentValue : 0,
+    currentOperator : '',
     calcDisplay : document.getElementById('calcDisplay'),
+    calcOps : document.getElementById('calcOps'),
     calcButtons : document.querySelector('.calculator-nums'),
-    clearDisplay() {
-        calcDisplay.innerHTML = 0;
+    clear() {
+        calculator.calcDisplay.innerHTML = 0;
     },
-    undoDisplay() {
-        const currentVal = calcDisplay.innerHTML;
+    undo() {
+        const currentVal = calculator.calcDisplay.innerHTML;
         const newVal = currentVal.slice(0, currentVal.length - 1 );
-        calcDisplay.innerHTML = newVal;
+        calculator.calcDisplay.innerHTML = newVal;
     },
-    updateDisplay(val) {
-        calcDisplay.innerHTML = (calcDisplay.innerHTML === '0') ? val : (calcDisplay.innerHTML + val);
+    compute(val) {
+        if (val === '+') {
+            this.currentOperator = 'add'
+        } else if ( val === '-' ) {
+            this.currentOperator = 'subtract'
+        } else if (val === 'X') {
+            this.currentOperator = 'multiply'
+        } else {
+            this.currentOperator = 'divide'
+        }
+    },
+    update(val) {
+        this.calcDisplay.innerHTML = (this.calcDisplay.innerHTML === '0') ? val : (this.calcDisplay.innerHTML + val);
     }
 }
 
@@ -20,17 +33,46 @@ let calculator = {
 calculator.calcButtons.addEventListener('click', function(e) {
     let buttonVal = e.target.innerHTML;
     // Prevent Accidential Drag/Drop
-    if (buttonVal.length === 1) {
-        if (e.target.innerHTML === 'C') {
-            calculator.clearDisplay();
-        } else if (e.target.innerHTML == '&lt;-') {
-            calculator.undoDisplay();
-        } else {
-            calculator.updateDisplay(buttonVal);
-        }
+    if (buttonVal.length === 1) {  
+        (e.target.innerHTML === 'C') ? calculator.clear() :  calculator.update(buttonVal);  // Determine action based on button label
+    } else if (e.target.innerHTML == '&lt;-') {
+        calculator.undo();
     }
 });
 
 // Event Listener for Operator Buttons
+calculator.calcOps.addEventListener('click', function(e) {
+    let buttonVal = e.target.innerHTML;
+    if (buttonVal === '=') {
+        // Store Result
+        let result = 0; 
+        // Turn strings into numbers
+        const currentValueInt = parseInt(calculator.currentValue);
+        const displayValueInt = parseInt(calculator.calcDisplay.innerHTML);
+        // Execute math based on calculator's current operatotr
+        if (calculator.currentOperator === 'add') {
+            result = currentValueInt + displayValueInt;
+        }
+        else if (calculator.currentOperator === 'subtract') {
+            result = currentValueInt - displayValueInt;
+        }
+        else if (calculator.currentOperator === 'multiply') {
+            result = currentValueInt * displayValueInt;
+        }
+        else {
+            result = currentValueInt / displayValueInt;
+        }
+        // Show Result
+        calculator.currentValue = result;
+        calculator.calcDisplay.innerHTML = result;
 
+    }  else {
+        calculator.compute(e.target.innerHTML);
+    }
+    if (calculator.calcDisplay.innerHTML !== '0' && buttonVal !== '=') {
+        calculator.currentValue = calculator.calcDisplay.innerHTML;
+        calculator.clear();
+    }
+
+});
 
